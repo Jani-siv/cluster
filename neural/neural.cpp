c807546ec2e024bea3017c0c1b88f8b3a202942d
Jani-siv <info@kengityspaja.fi>
2021-04-05 13:11:17 +0300

    added input class

--- neural/neural.cpp
+++ neural/neural.cpp
@@ -13,12 +13,15 @@ void neural::calculateOutput()
     auto start = std::chrono::high_resolution_clock::now();
     double temp;
     this->setInput();
+    for(int k = 0; k < this->inputClass.size(); k++)
+    {
+    // one for loop more for multiple inputs
     for(int i=0; i < this->weights.size(); i++)
     {
         for(int j = 0; j < weights[i].size(); j++)
         {
        // std::cout<<"["<<this->weights[i][j]<<"] ";
-        this->output[i] = this->output[i] + this->inputClass[0]->getData(j) * this->weights[i][j];
+        this->output[i] = this->output[i] + this->inputClass[k]->getData(j) * this->weights[i][j];
         }
         this->output[i] += this->biases[i];
       std::cout<<std::endl;
@@ -30,7 +33,7 @@ void neural::calculateOutput()
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout<<duration.count()<<"microseconds"<<std::endl;
-
+    }
 }
 
 //creating new class and push it in vector
